51dcaf488d0fe6c121326a927c50eeac962839d5
zyyang <adleyyang.cn@gmail.com>
2015-03-23 23:04:54 +0800

    hbase

--- src/main/scala/com/demo/hbase/client.scala
+++ src/main/scala/com/demo/hbase/client.scala
@@ -1,6 +1,7 @@
 package com.demo.hbase
 
 import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.hadoop.hbase.client.HBaseAdmin
 
 /**
  * Project Name:sample
@@ -12,9 +13,16 @@ object client extends App{
 
   val QUORUM = "192.168.86.4,192.168.86.14,192.168.86.15"
   val CLIENTPORT = "2181"
-  val conf  = new HBaseConfiguration()
+  val configuration = HBaseConfiguration.create()
+  val MASTER = "192.168.86.16:600000"
+  configuration.set("hbase.zookeeper.property.clientPort", CLIENTPORT)
+  configuration.set("hbase.zookeeper.quorum", QUORUM)
+  configuration.set("hbase.master", MASTER)
 
 
+  def createTable(tableName : String)={
+    val hBaseAdmin = new HBaseAdmin(configuration)
+  }
 
 
 }
