486fed7651c66af3ab91437af2ef8f16885f171d
adley-yang <adleyyang.cn@gmail.com>
2015-03-25 17:01:28 +0800

    hbase

--- src/main/scala/com/demo/hbase/client.scala
+++ src/main/scala/com/demo/hbase/client.scala
@@ -1,6 +1,8 @@
 package com.demo.hbase
 
-import org.apache.hadoop.hbase.HBaseConfiguration
+import java.io.IOException
+
+import org.apache.hadoop.hbase._
 import org.apache.hadoop.hbase.client.HBaseAdmin
 
 /**
@@ -9,20 +11,45 @@ import org.apache.hadoop.hbase.client.HBaseAdmin
  * Date:15-3-23 下午4:16
  * Copyright (c) 2015, adleyyang.cn@gmail.com All Rights Reserved.
  */
-object client extends App{
+object client extends App {
 
-  val QUORUM = "192.168.86.4,192.168.86.14,192.168.86.15"
+  val QUORUM = "bd-002,bd-003,bd-004"
   val CLIENTPORT = "2181"
   val configuration = HBaseConfiguration.create()
-  val MASTER = "192.168.86.16:600000"
+  val MASTER = "bd-001:60000"
   configuration.set("hbase.zookeeper.property.clientPort", CLIENTPORT)
   configuration.set("hbase.zookeeper.quorum", QUORUM)
   configuration.set("hbase.master", MASTER)
 
 
-  def createTable(tableName : String)={
-    val hBaseAdmin = new HBaseAdmin(configuration)
+  def createTable(tableName: String, cols: Array[String]) = {
+    try {
+      val hBaseAdmin = new HBaseAdmin(configuration)
+      /*if (hBaseAdmin.tableExists(tableName)) {
+        println("此表已经存在.......");
+      } else {*/
+        val table = new HTableDescriptor(tableName);
+        for (c <- cols) {
+          val col = new HColumnDescriptor(c); //列簇名
+          table.addFamily(col); //添加到此表中
+        }
+      //hBaseAdmin.createTable(table) //创建一个表
+        println(hBaseAdmin.listTableNames())
+        hBaseAdmin.close()
+        println("创建表成功!")
+     // }
+    } catch {
+      case e: MasterNotRunningException => println("创建表异常：" + e)
+      case e: ZooKeeperConnectionException => println("创建表异常：" + e)
+      case e: IOException => println("创建表异常：" + e)
+      case e: Exception => println("创建表异常：" + e)
+    }
   }
+
+
+  println("start create table")
+  //创建表
+  createTable("test_1", Array("name", "age"))
 
 
 }
